5d89b4989b7a6f4cd7702aeb5bc5c5f645214d25
Lance Taylor <lance.taylor98@hotmail.com>
2019-04-15 11:05:09 -0600

    Adds correct link for the rules page navigaion to index.js. Also adds rules for the game on the Rules page

---
+++ pages/rules.js
@@ -0,0 +1,15 @@
+export default function Rules() {
+  return (
+    <div>
+      <h1>Rules for the Numventure</h1>
+      <p>Numventure is a board game based in your browser where you can learn some cool number trivia.</p>
+      <h2>Rules for the Numventure</h2>
+      <p>1) Roll a dice.</p>
+      <p>2) You will get a question.</p>
+      <p>3) Place your answer in the box provided.</p>
+      <p>4) If you are correct you will see your color-piece advance.</p>
+      <p>5) If you are incorrect you will see your color-piece stay in the same spot.</p>
+      <p>6) The game ends when you manage to get to the final square. You can overshoot and win.</p>
+    </div>
+  )
+}
