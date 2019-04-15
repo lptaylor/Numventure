ea2c5c5b34664349bc0c6c3dba7533003891906d
Lance Taylor <lance.taylor98@hotmail.com>
2019-04-15 11:58:05 -0600

    Adds single board block to code.

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,14 @@ const Index = () => (
       <h1>Welcome to Numventure</h1>
       <h2>The game of random number knowledge!</h2>
       <h4>Please visit the Rules page to find out how the game works!</h4>
+      <i className='shape2'></i>
+      <style jsx>{`
+        .shape2 {
+          display:inline-block!important;
+          width:75px;
+          height:75px;
+          background-color:#666666}
+      `}</style>
   </div>
 )
 
