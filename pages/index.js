ae16fb10dba85aba361ed6eb494cda860754d97e
Lance Taylor <lance.taylor98@hotmail.com>
2019-04-15 07:23:35 -0600

    Inital commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// This is the Link API
+import Link from 'next/link'
+
+const Index = () => (
+  <div>
+    <Link href="/about">
+      <button>Go to About Page</button>
+    </Link>
+      <h1>Welcome to Numventure</h1>
+  </div>
+)
+
+export default Index
