a290942db64d23e6d7a17d4638ba1117378b8d52
Lance Taylor <lance.taylor98@hotmail.com>
2019-04-17 07:47:09 -0600

    Fixes deployment with Procfile

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app
       return handle(req, res)
     })
 
-    const PORT = process.env.PORT;
+    const PORT = process.env.PORT || 3000;
     server.listen(PORT, () => {
         console.log(`Our app is running on port ${ PORT }`);
     });
